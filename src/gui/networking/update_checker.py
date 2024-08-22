c0db6aa8600bc96a4ff03621a1c569db703bc224
Dodixon <rybarski.dominik97@gmail.com>
2024-08-22 08:28:04 +0200

    Aktualizacja

---
+++ src/gui/networking/update_checker.py
@@ -0,0 +1,115 @@
+import urllib.parse
+import urllib.request
+import json
+import ctypes
+import sys
+import re
+
+
+class VS_FIXEDFILEINFO(ctypes.Structure):
+    _fields_ = [
+        ("dwSignature", ctypes.c_uint32),
+        ("dwStrucVersion", ctypes.c_uint32),
+        ("dwFileVersionMS", ctypes.c_uint32),
+        ("dwFileVersionLS", ctypes.c_uint32),
+        ("dwProductVersionMS", ctypes.c_uint32),
+        ("dwProductVersionLS", ctypes.c_uint32),
+        ("dwFileFlagsMask", ctypes.c_uint32),
+        ("dwFileFlags", ctypes.c_uint32),
+        ("dwFileOS", ctypes.c_uint32),
+        ("dwFileType", ctypes.c_uint32),
+        ("dwFileSubtype", ctypes.c_uint32),
+        ("dwFileDateMS", ctypes.c_uint32),
+        ("dwFileDateLS", ctypes.c_uint32),
+    ]
+
+
+def check_app_update_status():
+    file_version, product_version = get_file_and_product_version()
+    app_version = get_latest_app_version()
+
+    app_version = list(map(int, re.sub(r'[^A-Za-z0-9]', ' ', app_version).split()))
+    file_version = list(map(int, re.sub(r'[^A-Za-z0-9]', ' ', file_version).split()))
+    product_version = list(map(int, re.sub(r'[^A-Za-z0-9]', ' ', product_version).split()))
+    
+    print(app_version, file_version, product_version, sep='\n')
+
+    if len(app_version) == 3:
+        app_version.append(0)
+
+    if app_version is False or not app_version:
+        return None
+    elif app_version == file_version or app_version == product_version:
+        return False  # Installed version is the same
+    elif app_version > file_version or app_version > product_version:
+        return False  # Installed version is the same
+    else:
+        return True  # Installed version is outdated
+
+def get_file_and_product_version(exe_path=sys.executable):
+    size = ctypes.windll.version.GetFileVersionInfoSizeW(exe_path, None)
+    if size == 0:
+        return None, None
+
+    res = ctypes.create_string_buffer(size)
+    ctypes.windll.version.GetFileVersionInfoW(exe_path, None, size, res)
+
+    r = ctypes.c_void_p()
+    l = ctypes.c_uint()
+
+    # Query the root block for the VS_FIXEDFILEINFO structure
+    ctypes.windll.version.VerQueryValueW(res, '\\', ctypes.byref(r), ctypes.byref(l))
+
+    info = ctypes.cast(r, ctypes.POINTER(VS_FIXEDFILEINFO)).contents
+
+    def print_vs_fixedfileinfo(info):
+        print("VS_FIXEDFILEINFO:")
+        print(f"  dwFileVersionMS: {info.dwFileVersionMS:#010x}")
+        print(f"  dwFileVersionLS: {info.dwFileVersionLS:#010x}")
+        print(f"  dwProductVersionMS: {info.dwProductVersionMS:#010x}")
+        print(f"  dwProductVersionLS: {info.dwProductVersionLS:#010x}")
+    #print_vs_fixedfileinfo(info)
+
+    # Extract file version numbers
+    file_version_ms = info.dwFileVersionMS
+    file_version_ls = info.dwFileVersionLS
+    file_version = (file_version_ms >> 16, file_version_ms & 0xffff, file_version_ls >> 16, file_version_ls & 0xffff)
+    file_version = ".".join(map(str, file_version)) if file_version else "Unknown file version"
+
+    # Extract product version numbers
+    product_version_ms = info.dwProductVersionMS
+    product_version_ls = info.dwProductVersionLS
+    product_version = (product_version_ms >> 16, product_version_ms & 0xffff, product_version_ls >> 16, product_version_ls & 0xffff)
+    product_version = ".".join(map(str, product_version)) if product_version else "Unknown file version"
+
+    return file_version, product_version
+
+def get_latest_app_version():
+    # Base URL and repository path
+    base_url = "https://api.github.com/repos/"
+    repo_path = "RybarskiDominik/GML-2021/releases/latest"
+    # Construct the full URL
+    #api_url_str = urllib.parse.urljoin(base_url, repo_path)
+
+    api_url_str = r"https://api.github.com/repos/RybarskiDominik/GML-2021/releases/latest"
+
+    # Prepare the request
+    headers = {
+        "Accept": "application/json"
+        #"Accept": "application/vnd.github.v3+json"
+    }
+
+    # Create a request object with headers
+    request = urllib.request.Request(api_url_str, headers=headers)
+
+    # Send the GET request and handle the response
+    with urllib.request.urlopen(request) as response:
+        if response.status == 200:
+            data = json.loads(response.read().decode())
+            return data["tag_name"]
+        else:
+            print(f"Request failed with status code: {response.status}")
+            return False
+
+if __name__ == "__main__":
+    print(check_app_update_status())
---
+++ src/gui/settings.py
@@ -0,0 +1,176 @@
+from PySide6.QtWidgets import (QApplication, QMainWindow, QCheckBox,
+                               QApplication, QMainWindow, QLabel,
+                               QFrame, QGroupBox, QVBoxLayout, QWidget, QPushButton)
+from PySide6.QtCore import Qt,QRect, QSettings, QCoreApplication
+from PySide6.QtGui import QIcon
+from PySide6 import QtWidgets
+
+try:
+    from .networking.update_checker import check_app_update_status
+except:
+    from networking.update_checker import check_app_update_status
+
+from pathlib import Path
+import logging
+import sys
+import os
+
+
+class SettingsWindow(QMainWindow):
+    def __init__(self, dark_mode_enabled=None):
+        super(SettingsWindow,self).__init__()
+        app_update_status = None
+        self.settings = QSettings('GML', 'GML Reader')
+
+        if dark_mode_enabled:
+            self.setStyleSheet("""
+            QCheckBox {
+                color: #ffffff;
+                }
+            QGroupBox {
+                color: #ffffff;
+                }
+            """)
+
+        self.setFixedSize(350, 400)
+        self.setWindowTitle('Preferencje')
+        self.setWindowIcon(QIcon(r'gui\Icons\GML.ico'))
+        
+        self.config()
+        
+        self.init_UI()
+        
+    def config(self):
+        
+        save_value = []
+
+        for i in self.settings.allKeys():
+            value = self.settings.value(i)
+            save_value.append((i, value))
+        self.save_value = save_value  # save settings for recovery
+        if self.save_value:
+            print(self.save_value)
+    
+    def init_UI(self):
+        group_main = QGroupBox("GML", self)
+        group_main.setGeometry(5, 0, 190, 160)
+        
+        group_app = QGroupBox("Wygląd", self)
+        group_app.setGeometry(200, 0, 145, 160)
+
+        group_map = QGroupBox("Mapa", self)
+        group_map.setGeometry(5, 160, 190, 200)
+
+        group_points = QGroupBox("Punkty", self)
+        group_points.setGeometry(200, 160, 145, 165)
+
+        self.button_check_update = QtWidgets.QPushButton('Check Update', self)
+        self.button_check_update.setGeometry(200, 330, 145, 28)
+        self.button_check_update.clicked.connect(self.check_update)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        #layout.addWidget()
+        group_main.setLayout(layout)
+
+
+        self.dark_mode = QCheckBox('DarkMode', self)
+        if self.settings.value('DarkMode', False, type=bool) == True:
+            self.dark_mode.setChecked(True)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        layout.addWidget(self.dark_mode)
+        group_app.setLayout(layout)
+
+
+        self.map_flag = QCheckBox('Mapa zawsze na wierzchu.', self)
+        if self.settings.value('MapStaysOnTopHint', False, type=bool) == True:
+            self.map_flag.setChecked(True)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        layout.addWidget(self.map_flag)
+        group_map.setLayout(layout)
+
+        self.points_id = QCheckBox('Pełne ID', self)
+        if self.settings.value('FullID', False, type=bool) == True:
+            self.points_id.setChecked(True)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignTop)
+        layout.addWidget(self.points_id)
+        group_points.setLayout(layout)
+
+        self.button_Reset = QtWidgets.QPushButton(self)
+        self.button_Reset.setText("Reset Settings")
+        self.button_Reset.setGeometry(5, 365, 95, 30)
+        self.button_Reset.clicked.connect(self.reset)
+
+        self.button_Reset = QtWidgets.QPushButton(self)
+        self.button_Reset.setText("Zapisz")
+        self.button_Reset.setGeometry(155, 365, 95, 30)
+        self.button_Reset.clicked.connect(self.zapisz)
+
+        self.button_Reset = QtWidgets.QPushButton(self)
+        self.button_Reset.setGeometry(250, 365, 95, 30)
+        self.button_Reset.setText("Anuluj")
+        self.button_Reset.clicked.connect(self.anuluj)
+
+    def check_update(self):
+        try:
+            app_update_status = check_app_update_status()
+        except Exception as e:
+            logging.exception(e)
+        print(app_update_status)
+        if app_update_status == True:  
+            self.button_check_update.setText("Dostępna jest aktualizacja.") 
+            self.button_check_update.setStyleSheet('background-color: "#975D9F"')    
+        if app_update_status == False:
+            self.button_check_update.setText("Wersja aktualna.")
+            self.button_check_update.setStyleSheet('background-color: "#77C66E"')
+        if app_update_status == None:
+            self.button_check_update.setText("Błąd!")
+            self.button_check_update.setStyleSheet('background-color: "#ab2c0c"')
+
+    def reset(self):
+        self.settings.clear()
+        self.close()
+
+    def zapisz(self):
+        self.settings.setValue("DarkMode", self.dark_mode.isChecked())
+        self.settings.setValue("MapStaysOnTopHint", self.map_flag.isChecked())
+        self.settings.setValue("FullID", self.points_id.isChecked())
+        #settings.setValue("windowSize", self.size())
+        self.close()
+
+    def anuluj(self):
+        for i, k in self.save_value:
+            self.settings.setValue(i, k)
+        self.close()
+
+
+if __name__ == '__main__':
+    app = QApplication( sys.argv )
+    
+    settings = QSettings('GML', 'GML Reader')
+    dark_mode_enabled = settings.value('DarkMode', False, type=bool)
+    
+    try:
+        if settings.value('DarkMode', True, type=bool):
+            app.setStyleSheet(Path('gui/Stylesheets/Darkmode.qss').read_text())        
+        else:
+            app.setStyleSheet("""
+            QGraphicsView {
+                border: none;
+                background: transparent;
+            }
+            """)
+            
+    except Exception as e:
+            logging.exception(e)
+            print(e)
+
+    ConfigWindow = SettingsWindow(dark_mode_enabled)
+    ConfigWindow.show()
+    sys.exit(app.exec())
